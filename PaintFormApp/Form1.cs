ca664fa992d9008c5f85c157a1c06ec591f1a047
KseniaCherepanova <kseniiacherepanova07@gmail.com>
2025-06-04 16:11:37 +0500

    task 15 is completed

---
+++ PaintFormApp/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaintFormApp
+{
+    public partial class Form1 : Form
+    {
+        Graphics canvas;
+        public Form1()
+        {
+            InitializeComponent();
+
+            canvas = CreateGraphics();
+        }
+
+        private void Form1_Load(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+        }
+    }
+}
